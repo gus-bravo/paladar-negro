15fc79bcd7ebb40b9754c8f44550370b36f68848
gus-bravo <gabrielbravo761@gmail.com>
2025-06-16 14:44:49 -0300

    Mejoras en accesibilidad, performance y estructura de la grilla de productos

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,15 @@
 lucide.createIcons();
 
 // Manejo del botón menú hamburguesa
-const menuBtn = document.getElementById('menu-btn');
-const navLinks = document.getElementById('nav-links');
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
 let menuOpen = false;
 
-menuBtn.addEventListener('click', () => {
-  navLinks.classList.toggle('hidden');
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("hidden");
   menuOpen = !menuOpen;
 
-  menuBtn.innerHTML = `<i data-lucide="${menuOpen ? 'x' : 'menu'}"></i>`;
+  menuBtn.innerHTML = `<i data-lucide="${menuOpen ? "x" : "menu"}"></i>`;
   lucide.createIcons();
 });
 
